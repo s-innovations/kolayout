917c332b27d4f4bf8d0d96cc210aba1b5b1d9b30
Poul Kjeldager Sørensen <pks@s-innovations.net>
2016-10-16 20:09:32 +0200

    added coveralls

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ﻿
 # KoLayout
+[![NPM version][npm-image]][npm-url]
+[![Downloads][downloads-image]][downloads-url]
 [![The MIT License](https://img.shields.io/badge/license-MIT-orange.svg?style=flat-square)](http://opensource.org/licenses/MIT)
 [![Visual Studio Team services](https://img.shields.io/vso/build/sinnovations/40c16cc5-bf99-47d4-a814-56c38cc0ea24/22.svg?style=flat-square&label=build:%20KoLayout)]()
+[![Coveralls](https://img.shields.io/coveralls/s-innovations/kolayout.svg?style=flat-square)]()
 [![GitHub](https://img.shields.io/github/release/s-innovations/kolayout.svg?style=flat-square)](https://github.com/s-innovations/kolayout/releases)
 [![David](https://img.shields.io/david/s-innovations/kolayout.svg?style=flat-square)](https://david-dm.org/s-innovations/kolayout)
 [![David](https://img.shields.io/david/dev/s-innovations/kolayout.svg?style=flat-square)](https://david-dm.org/s-innovations/kolayout?type=dev)
@@ -70,3 +73,11 @@ assuming you copyed over npm package to libs in your gruntfile.js
     }
   }
 ```
+
+
+
+
+[npm-image]: https://img.shields.io/npm/v/kolayout.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/kolayout
+[downloads-image]: http://img.shields.io/npm/dm/kolayout.svg?style=flat-square
+[downloads-url]: https://npmjs.org/package/kolayout
--- gruntfile.js
+++ gruntfile.js
@@ -25,6 +25,12 @@
                                     dir: 'build/coverage'
                                 }
                             },
+                             {
+                                 type: "lcov",
+                                 options: {
+                                     dir: 'build/coverage/lcov'
+                                 }
+                             },
                             {
                             type: 'cobertura',
                             options: {
@@ -60,9 +66,27 @@
                 }
             }
         },
+        coveralls: {
+            // Options relevant to all targets
+            options: {
+                // When true, grunt-coveralls will only print a warning rather than
+                // an error, to prevent CI builds from failing unnecessarily (e.g. if
+                // coveralls.io is down). Optional, defaults to false.
+                force: false
+            },
+
+            your_target: {
+                // LCOV coverage file (can be string, glob or array)
+                src: 'build/coverage/lcov/*.info',
+                options: {
+                    // Any options for just this target
+                }
+            },
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-coveralls');
 
     grunt.task.registerTask("tests", ["jasmine:tests"]);
 };
