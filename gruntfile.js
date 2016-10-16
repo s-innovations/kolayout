17ca486c0d5bcbd70579aeada05c6f787e7f70a7
Poul Kjeldager Sørensen <pks@s-innovations.net>
2016-10-16 03:49:18 +0200

    added unit test

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+﻿module.exports = function (grunt) {
+    
+
+    grunt.initConfig({
+        jasmine: {
+            tests: {
+                //  src: ['wwwroot/libs/AscendLimitation/**/*.js'],
+                phantomjs: {
+                    resourceTimeout: 25000,
+                },
+                options: {
+                    specs: ['tests/*.js'],
+                    //     vendor: "node_modules/**/*.js",
+                    template: require('grunt-template-jasmine-requirejs'),
+                    templateOptions: {
+                        requireConfig: {
+                            shim: {
+                               
+                            },
+                            baseUrl: '.',
+                            paths: {
+                               
+                            },
+                            packages: [
+                                 {
+                                     name: "kolayout",
+                                     main: "index",
+                                     location: "dist/src"
+                                 },
+                            ]
+                        }
+                    }
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    
+    grunt.task.registerTask("tests", ["jasmine:tests"]);
+};
---
+++ tests/koLayoutSuite.ts
@@ -0,0 +1,19 @@
+﻿
+import {isDefined } from "kolayout/utils/isDefined";
+
+describe("koLayoutSuite", function () {
+    it("isDefined spec", function () {
+
+        expect(isDefined(undefined)).toBe(false);
+        expect(isDefined(null)).toBe(false);
+        expect(isDefined("a")).toBe(true);
+        expect(isDefined(false)).toBe(true);
+        expect(isDefined(0)).toBe(true);
+
+    });
+});
+
+
+
+
+
